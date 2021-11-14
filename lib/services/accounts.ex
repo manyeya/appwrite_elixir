560d7d2b69fa15511a78d8770a360a1eb8f2dfea
Manyeya <siemakhutso@gmail.com>
2021-11-14 12:15:20 +0200

    added a accounts service details

--- lib/services/accounts.ex
+++ lib/services/accounts.ex
@@ -1,6 +1,15 @@
 defmodule AppwriteElixir.Services.Accounts do
   alias AppwriteElixir.Services.Generics
 
+  @doc """
+  The Account service allows you to authenticate and manage a user account.
+  You can use the account service to update user information,
+  retrieve the user sessions across different devices,
+  and fetch the user security logs with his or her recent activity.
+  You can authenticate the user account by using multiple sign-in methods available.
+  Once the user is authenticated, a new session object will be created to allow the user to access his or her private data and settings.
+  """
+
   @host Application.get_env(:appwrite_elixir, :host)
 
   def get_all_users() do
@@ -17,5 +26,12 @@ defmodule AppwriteElixir.Services.Accounts do
 
   def get_user_logs(user_id) do
     Generics.get("http://#{@host}/v1/users/#{user_id}/logs")
+  end
+
+  def create_user() do
+    Generics.post("http://#{@host}/v1/account", %{
+      "email" => 'email@example.com',
+      "password" => 'password'
+    })
   end
 end
